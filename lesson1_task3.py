22613f6daca904b404b9ef18f2486e01d9cc4be3
SerJ <ury_047@inbox.ru>
2021-02-22 21:51:47 +0300

    GB_lesson1 (task1,task2, task3)

---
+++ lesson1_task3.py
@@ -0,0 +1,9 @@
+a = int(input("Enter number: "))
+if a == 1:
+    print(a, "процент")
+if a == 2 or a== 3 or a == 4:
+    print(a, "процентa")
+if a == 5 or a == 6 or a == 7 or a == 8 or a == 9 or a == 10 or a == 11 or a == 12 or a == 13 or a == 14 or a == 15 or a == 16 or a == 17 or a == 18 or a == 19 or a == 20:
+    print(a, "процентов")
+elif a > 20:
+    print("Введите другое число")
